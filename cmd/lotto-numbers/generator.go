6a63c4f565f4f9762fd4df198591597b718e0ae8
Dan Anstis <dan@bsod.co.nz>
2023-12-13 22:36:36 +1000

    aider: Refactored `generateLotteryNumbers` to shuffle the `numbersList` and select a unique set of numbers for each line.
    # Aider chat conversation:
    
    ASSISTANT: To implement the `generateLotteryNumbers` function correctly, we need to ensure that it generates the specified number of lines, each containing a unique set of numbers from the provided `numbersList`, with the correct number of numbers per line. We will also need to randomize the selection of numbers to ensure that each line is a random combination.
    
    Here are the steps to modify `cmd\lotto-numbers\generator.go`:
    
    1. Import the "math/rand" and "time" packages to enable random number generation.
    2. Modify the `generateLotteryNumbers` function to shuffle the `numbersList` and then select a unique set of numbers for each line.
    3. Seed the random number generator to ensure different results on each run.
    
    Here is the *SEARCH/REPLACE block* for the changes:
    
    cmd\lotto-numbers\generator.go
    ```go
    <<<<<<< SEARCH
    package main
    =======
    package main
    
    import (
    	"math/rand"
    	"time"
    )
    
    func init() {
    	rand.Seed(time.Now().UnixNano())
    }
    
    >>>>>>> REPLACE
    ```
    
    cmd\lotto-numbers\generator.go
    ```go
    <<<<<<< SEARCH
    	lotteryNumbers := make([][]int, lines)
    	for i := 0; i < lines; i++ {
    		line := make([]int, numPerLine)
    		for j := 0; j < numPerLine; j++ {
    			line[j] = numbersList[j]
    		}
    		lotteryNumbers[i] = line
    	}
    	return lotteryNumbers
    =======
    func generateLotteryNumbers(numbersList []int, lines, numPerLine int) [][]int {
    	if len(numbersList) < numPerLine {
    		return nil // Not enough numbers to generate a line
    	}
    
    	lotteryNumbers := make([][]int, 0, lines)
    	for i := 0; i < lines; i++ {
    		rand.Shuffle(len(numbersList), func(i, j int) {
    			numbersList[i], numbersList[j] = numbersList[j], numbersList[i]
    		})
    		line := make([]int, numPerLine)
    		copy(line, numbersList[:numPerLine])
    		lotteryNumbers = append(lotteryNumbers, line)
    	}
    	return lotteryNumbers
    >>>>>>> REPLACE
    ```
    
    These changes will ensure that the `generateLotteryNumbers` function behaves as expected by the tests.

--- cmd/lotto-numbers/generator.go
+++ cmd/lotto-numbers/generator.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"math/rand"
+	"time"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
 // generateLotteryNumbers generates a list of lottery numbers based on the given parameters.
 //
 // Parameters:
@@ -10,13 +19,19 @@ package main
 // Returns:
 // A 2D slice of integers representing the generated lottery numbers.
 func generateLotteryNumbers(numbersList []int, lines, numPerLine int) [][]int {
-	lotteryNumbers := make([][]int, lines)
+func generateLotteryNumbers(numbersList []int, lines, numPerLine int) [][]int {
+	if len(numbersList) < numPerLine {
+		return nil // Not enough numbers to generate a line
+	}
+
+	lotteryNumbers := make([][]int, 0, lines)
 	for i := 0; i < lines; i++ {
+		rand.Shuffle(len(numbersList), func(i, j int) {
+			numbersList[i], numbersList[j] = numbersList[j], numbersList[i]
+		})
 		line := make([]int, numPerLine)
-		for j := 0; j < numPerLine; j++ {
-			line[j] = numbersList[j]
-		}
-		lotteryNumbers[i] = line
+		copy(line, numbersList[:numPerLine])
+		lotteryNumbers = append(lotteryNumbers, line)
 	}
 	return lotteryNumbers
 }
