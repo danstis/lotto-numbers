6b476ba68b1f67c4beb4c69b6cf935fdf2417564
Dan Anstis <dan@bsod.co.nz>
2023-12-13 22:26:45 +1000

    aider: Split the TestGenerateLotteryNumbers function into smaller test cases.
    # Aider chat conversation:
    
    USER: split the TestGenerateLotteryNumbers function into smaller test cases
    
    ASSISTANT: To split the `TestGenerateLotteryNumbers` function into smaller test cases, we will create separate test functions for each aspect of the `generateLotteryNumbers` function that we want to test. We will create tests for the following:
    
    1. Correct number of lines generated.
    2. Correct number of numbers per line.
    3. Uniqueness of numbers in each line.
    4. All numbers in a line are from the `numbersList`.
    
    Here are the *SEARCH/REPLACE* blocks for each of the new test functions:
    
    cmd\lotto-numbers\generator_test.go
    ```go
    <<<<<<< SEARCH
    func TestGenerateLotteryNumbers(t *testing.T) {
    	numbersList := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
    	lines := 3
    	numPerLine := 5
    
    	generatedNumbers := generateLotteryNumbers(numbersList, lines, numPerLine)
    
    	// Test for correct number of lines and numbers per line
    	if len(generatedNumbers) != lines {
    		t.Errorf("Expected %d lines, got %d", lines, len(generatedNumbers))
    	}
    	for _, line := range generatedNumbers {
    		if len(line) != numPerLine {
    			t.Errorf("Expected %d numbers per line, got %d", numPerLine, len(line))
    		}
    
    		// Test for uniqueness of numbers in each line
    		numMap := make(map[int]bool)
    		for _, num := range line {
    			if numMap[num] {
    				t.Errorf("Duplicate number found in a line: %d", num)
    			}
    			numMap[num] = true
    
    			// Test that number is in numbersList
    			if !contains(numbersList, num) {
    				t.Errorf("Number %d in generated line is not in the original numbersList", num)
    			}
    		}
    	}
    }
    =======
    func TestGenerateLotteryNumbers_CorrectNumberOfLines(t *testing.T) {
    	numbersList := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
    	lines := 3
    	numPerLine := 5
    
    	generatedNumbers := generateLotteryNumbers(numbersList, lines, numPerLine)
    
    	if len(generatedNumbers) != lines {
    		t.Errorf("Expected %d lines, got %d", lines, len(generatedNumbers))
    	}
    }
    
    func TestGenerateLotteryNumbers_CorrectNumbersPerLine(t *testing.T) {
    	numbersList := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
    	lines := 3
    	numPerLine := 5
    
    	generatedNumbers := generateLotteryNumbers(numbersList, lines, numPerLine)
    
    	for _, line := range generatedNumbers {
    		if len(line) != numPerLine {
    			t.Errorf("Expected %d numbers per line, got %d", numPerLine, len(line))
    		}
    	}
    }
    
    func TestGenerateLotteryNumbers_UniqueNumbersInLine(t *testing.T) {
    	numbersList := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
    	lines := 3
    	numPerLine := 5
    
    	generatedNumbers := generateLotteryNumbers(numbersList, lines, numPerLine)
    
    	for _, line := range generatedNumbers {
    		numMap := make(map[int]bool)
    		for _, num := range line {
    			if numMap[num] {
    				t.Errorf("Duplicate number found in a line: %d", num)
    			}
    			numMap[num] = true
    		}
    	}
    }
    
    func TestGenerateLotteryNumbers_NumbersFromList(t *testing.T) {
    	numbersList := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
    	lines := 3
    	numPerLine := 5
    
    	generatedNumbers := generateLotteryNumbers(numbersList, lines, numPerLine)
    
    	for _, line := range generatedNumbers {
    		for _, num := range line {
    			if !contains(numbersList, num) {
    				t.Errorf("Number %d in generated line is not in the original numbersList", num)
    			}
    		}
    	}
    }
    >>>>>>> REPLACE
    ```
    
    This will create four new test functions, each focusing on a specific aspect of the `generateLotteryNumbers` function.

--- cmd/lotto-numbers/generator_test.go
+++ cmd/lotto-numbers/generator_test.go
@@ -5,31 +5,59 @@ import (
 	"testing"
 )
 
-func TestGenerateLotteryNumbers(t *testing.T) {
+func TestGenerateLotteryNumbers_CorrectNumberOfLines(t *testing.T) {
 	numbersList := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 	lines := 3
 	numPerLine := 5
 
 	generatedNumbers := generateLotteryNumbers(numbersList, lines, numPerLine)
 
-	// Test for correct number of lines and numbers per line
 	if len(generatedNumbers) != lines {
 		t.Errorf("Expected %d lines, got %d", lines, len(generatedNumbers))
 	}
+}
+
+func TestGenerateLotteryNumbers_CorrectNumbersPerLine(t *testing.T) {
+	numbersList := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	lines := 3
+	numPerLine := 5
+
+	generatedNumbers := generateLotteryNumbers(numbersList, lines, numPerLine)
+
 	for _, line := range generatedNumbers {
 		if len(line) != numPerLine {
 			t.Errorf("Expected %d numbers per line, got %d", numPerLine, len(line))
 		}
+	}
+}
 
-		// Test for uniqueness of numbers in each line
+func TestGenerateLotteryNumbers_UniqueNumbersInLine(t *testing.T) {
+	numbersList := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	lines := 3
+	numPerLine := 5
+
+	generatedNumbers := generateLotteryNumbers(numbersList, lines, numPerLine)
+
+	for _, line := range generatedNumbers {
 		numMap := make(map[int]bool)
 		for _, num := range line {
 			if numMap[num] {
 				t.Errorf("Duplicate number found in a line: %d", num)
 			}
 			numMap[num] = true
+		}
+	}
+}
 
-			// Test that number is in numbersList
+func TestGenerateLotteryNumbers_NumbersFromList(t *testing.T) {
+	numbersList := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	lines := 3
+	numPerLine := 5
+
+	generatedNumbers := generateLotteryNumbers(numbersList, lines, numPerLine)
+
+	for _, line := range generatedNumbers {
+		for _, num := range line {
 			if !contains(numbersList, num) {
 				t.Errorf("Number %d in generated line is not in the original numbersList", num)
 			}
