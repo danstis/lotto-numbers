1b151ef600d40e6135669066831be08318482760
Dan Anstis <dan@bsod.co.nz>
2023-12-14 21:13:55 +1000

    Add HTTP server setup using Gorilla Mux and delete redundant server.go file

--- cmd/lotto-numbers/main.go
+++ cmd/lotto-numbers/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"log"
+	"net/http"
 
+	"github.com/danstis/lotto-numbers/internal/handlers" // Import the handlers package
 	"github.com/danstis/lotto-numbers/internal/version"
+	"github.com/gorilla/mux"
 )
 
 // Main entry point for the app.
@@ -12,4 +15,14 @@ func main() {
 
 	num := generateLotteryNumbers([]int{1, 1, 1, 1, 1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29}, 5, 5)
 	log.Printf("Numbers: %v", num)
+
+	// Set up the HTTP server using Gorilla Mux
+	r := mux.NewRouter()
+	r.HandleFunc("/lottery-numbers", handlers.GetLotteryNumbers).Methods("GET")
+
+	// Start the server
+	log.Println("Starting server on :8080")
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		log.Fatal("ListenAndServe error: ", err)
+	}
 }
