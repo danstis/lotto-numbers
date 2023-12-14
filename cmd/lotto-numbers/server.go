ede328ccc120e57b466790cc87f794fd4c1110c8
Dan Anstis <dan@bsod.co.nz>
2023-12-14 21:11:40 +1000

    fix: update imports

--- cmd/lotto-numbers/server.go
+++ cmd/lotto-numbers/server.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-    "log"
-    "net/http"
-    "github.com/gorilla/mux"
-    "internal/handlers" // Import the handlers package
+	"log"
+	"net/http"
+
+	"github.com/danstis/lotto-numbers/internal/handlers" // Import the handlers package
+	"github.com/gorilla/mux"
 )
 
 func main() {
