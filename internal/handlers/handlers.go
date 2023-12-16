c14d1b9d4ba26e6d9f5ad7c228f59d29c40554fb
Dan Anstis <dan@bsod.co.nz>
2023-12-16 10:41:50 +1000

    Refactor number of lines and numbers per line variables in GetLotteryNumbers function.

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -11,8 +11,8 @@ import (
 func GetLotteryNumbers(w http.ResponseWriter, r *http.Request) {
 	// Define your parameters for the lottery numbers
 	numbersList := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
-	lines := 5       // The number of lines of lottery numbers to generate
-	numPerLine := 6  // The number of numbers per line
+	lines := 5      // The number of lines of lottery numbers to generate
+	numPerLine := 6 // The number of numbers per line
 
 	// Call the generateLotteryNumbers function with the defined parameters
 	generatedNumbers := generateLotteryNumbers(numbersList, lines, numPerLine)
