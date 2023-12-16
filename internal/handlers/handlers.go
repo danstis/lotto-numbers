c22a14f72ff25e500386e74174f2e52c3b720a64
Dan Anstis <dan@bsod.co.nz>
2023-12-16 10:38:44 +1000

    aider: Updated the `GetLotteryNumbers` handler to use the `generateLotteryNumbers` function.
    # Aider chat conversation:
    
    USER: update the `GetLotteryNumbers` handler to use the `generateLotteryNumbers` function
    
    ASSISTANT: To update the `GetLotteryNumbers` handler to use the `generateLotteryNumbers` function, we need to modify the `internal\handlers\handlers.go` file. We will replace the placeholder implementation with a call to `generateLotteryNumbers` and handle the response accordingly.
    
    Here is the *SEARCH/REPLACE block* for the required changes:
    
    internal\handlers\handlers.go
    ```go
    <<<<<<< SEARCH
    // GetLotteryNumbers handles the request to generate lottery numbers.
    func GetLotteryNumbers(w http.ResponseWriter, r *http.Request) {
    	// This is a placeholder implementation.
    	// You should replace it with the actual logic to generate lottery numbers.
    	numbers := models.LotteryNumbers{
    		Lines: [][]int{{1, 2, 3, 4, 5, 6}},
    	}
    
    	w.Header().Set("Content-Type", "application/json")
    	json.NewEncoder(w).Encode(numbers)
    }
    =======
    // GetLotteryNumbers handles the request to generate lottery numbers.
    func GetLotteryNumbers(w http.ResponseWriter, r *http.Request) {
    	// Define your parameters for the lottery numbers
    	numbersList := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
    	lines := 5       // The number of lines of lottery numbers to generate
    	numPerLine := 6  // The number of numbers per line
    
    	// Call the generateLotteryNumbers function with the defined parameters
    	generatedNumbers := generateLotteryNumbers(numbersList, lines, numPerLine)
    
    	// Create a LotteryNumbers struct with the generated numbers
    	numbers := models.LotteryNumbers{
    		Lines: generatedNumbers,
    	}
    
    	// Set the Content-Type header and encode the LotteryNumbers struct to JSON
    	w.Header().Set("Content-Type", "application/json")
    	json.NewEncoder(w).Encode(numbers)
    }
    >>>>>>> REPLACE
    ```
    
    This change will make the `GetLotteryNumbers` handler generate lottery numbers using the `generateLotteryNumbers` function with the specified parameters. The generated numbers are then encoded to JSON and sent in the HTTP response. If you have specific parameters or error handling requirements, please let me know, and I can adjust the code accordingly.

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -9,12 +9,20 @@ import (
 
 // GetLotteryNumbers handles the request to generate lottery numbers.
 func GetLotteryNumbers(w http.ResponseWriter, r *http.Request) {
-	// This is a placeholder implementation.
-	// You should replace it with the actual logic to generate lottery numbers.
+	// Define your parameters for the lottery numbers
+	numbersList := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
+	lines := 5       // The number of lines of lottery numbers to generate
+	numPerLine := 6  // The number of numbers per line
+
+	// Call the generateLotteryNumbers function with the defined parameters
+	generatedNumbers := generateLotteryNumbers(numbersList, lines, numPerLine)
+
+	// Create a LotteryNumbers struct with the generated numbers
 	numbers := models.LotteryNumbers{
-		Lines: [][]int{{1, 2, 3, 4, 5, 6}},
+		Lines: generatedNumbers,
 	}
 
+	// Set the Content-Type header and encode the LotteryNumbers struct to JSON
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(numbers)
 }
