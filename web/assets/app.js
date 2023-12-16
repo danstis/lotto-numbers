e0c1ba48a3dad2740f22623be702ef0adabdd1e0
Dan Anstis <dan@bsod.co.nz>
2023-12-16 18:47:57 +1000

    feat: add web assets

---
+++ web/assets/app.js
@@ -0,0 +1,41 @@
+document
+  .getElementById("generateButton")
+  .addEventListener("click", function () {
+    // Get input values
+    let numLines = document.getElementById("numLines").value;
+    let numPerLine = document.getElementById("numPerLine").value;
+    let numbers = document.getElementById("numbers").value;
+
+    // Construct the API URL
+    let apiUrl = `https://lotto-numbers.fly.dev/numbers?lines=${numLines}&numPerLine=${numPerLine}&numbersList=${numbers}`;
+
+    // Fetch data from the API
+    fetch(apiUrl)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => displayNumbers(data))
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        displayError(error);
+      });
+  });
+
+function displayNumbers(data) {
+  const container = document.getElementById("numbersContainer");
+  container.innerHTML = ""; // Clear previous results
+
+  data.lines.forEach((line) => {
+    const lineElem = document.createElement("div");
+    lineElem.textContent = `Line: ${line.join(", ")}`;
+    container.appendChild(lineElem);
+  });
+}
+
+function displayError(error) {
+  const container = document.getElementById("numbersContainer");
+  container.innerHTML = `<div class="error">Error: ${error.message}</div>`;
+}
