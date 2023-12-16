b8613ea820449fce0cf397ac8df66bce35a189ec
Dan Anstis <dan@bsod.co.nz>
2023-12-16 11:42:58 +1000

    refactor: formatting

--- internal/models/models.go
+++ internal/models/models.go
@@ -3,5 +3,5 @@ package models
 // Define your models here. For example:
 // LotteryNumbers represents a set of generated lottery numbers.
 type LotteryNumbers struct {
-    Lines [][]int `json:"lines"`
+	Lines [][]int `json:"lines"`
 }
