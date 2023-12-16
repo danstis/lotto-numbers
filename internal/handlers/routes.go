6ae397dc7dcd208f4eff1315af3c6786fda380a3
Dan Anstis <dan@bsod.co.nz>
2023-12-16 18:54:04 +1000

    aider: Added a new route to serve static web content from the `/web/assets` folder.
    # Aider chat conversation:
    
    ASSISTANT: To add a new route to serve the static web content from the `/web/assets` folder, we need to modify the `internal/handlers/routes.go` file. We will use the `PathPrefix` and `Handler` functions from the `mux` package to serve files from a specified directory.
    
    Here is the *SEARCH/REPLACE* block for the changes:
    
    internal/handlers/routes.go
    ```go
    <<<<<<< SEARCH
    import (
    	"github.com/gorilla/mux"
    )
    
    // SetupRoutes configures the routes for the application.
    func SetupRoutes(r *mux.Router) {
    	r.HandleFunc("/numbers", GetLotteryNumbers).Methods("GET")
    }
    =======
    import (
    	"net/http"
    	"github.com/gorilla/mux"
    )
    
    // SetupRoutes configures the routes for the application.
    func SetupRoutes(r *mux.Router) {
    	r.HandleFunc("/numbers", GetLotteryNumbers).Methods("GET")
    	r.PathPrefix("/assets/").Handler(http.StripPrefix("/assets/", http.FileServer(http.Dir("web/assets"))))
    }
    >>>>>>> REPLACE
    ```
    
    This change adds a new route that will serve the static files located in the `web/assets` directory when the URL path starts with `/assets/`. The `http.StripPrefix` function is used to remove the `/assets/` prefix before the request is passed to the `http.FileServer`.

--- internal/handlers/routes.go
+++ internal/handlers/routes.go
@@ -1,10 +1,12 @@
 package handlers
 
 import (
+	"net/http"
 	"github.com/gorilla/mux"
 )
 
 // SetupRoutes configures the routes for the application.
 func SetupRoutes(r *mux.Router) {
 	r.HandleFunc("/numbers", GetLotteryNumbers).Methods("GET")
+	r.PathPrefix("/assets/").Handler(http.StripPrefix("/assets/", http.FileServer(http.Dir("web/assets"))))
 }
