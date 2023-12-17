389e7046e950ca7d5bdf0dfc9d3aad9612e420fa
Dan Anstis <dan@bsod.co.nz>
2023-12-17 11:51:12 +1000

    Add embedded file system for serving static files

--- internal/handlers/routes.go
+++ internal/handlers/routes.go
@@ -1,17 +1,35 @@
 package handlers
 
 import (
+	"embed"
+	"io/fs"
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
+//go:embed web/index.html web/assets/*
+var static embed.FS
+
 // SetupRoutes configures the routes for the application.
 func SetupRoutes(r *mux.Router) {
-	webDir := "../../web"
+	// Create a sub filesystem for the web directory.
+	webFs, err := fs.Sub(static, "web")
+	if err != nil {
+		log.Fatalf("Unable to create sub filesystem: %v", err)
+	}
+	// Create a sub filesystem for the assets directory.
+	assetsFs, err := fs.Sub(static, "web/assets")
+	if err != nil {
+		log.Fatalf("Unable to create sub filesystem for assets: %v", err)
+	}
+
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, webDir+"/index.html")
+		// Serve the index.html file from the sub filesystem
+		fileServer := http.FileServer(http.FS(webFs))
+		fileServer.ServeHTTP(w, r)
 	})
-	r.PathPrefix("/assets/").Handler(http.StripPrefix("/assets/", http.FileServer(http.Dir(webDir+"/assets"))))
+	r.PathPrefix("/assets/").Handler(http.StripPrefix("/assets/", http.FileServer(http.FS(assetsFs))))
 	r.HandleFunc("/numbers", GetLotteryNumbers).Methods("GET")
 }
