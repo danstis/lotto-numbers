de038f7e842bf254666f802488e8f8fc5defc3cb
Dan Anstis <dan@bsod.co.nz>
2023-12-17 13:42:54 +1000

    add tracing functionality

--- cmd/lotto-numbers/main.go
+++ cmd/lotto-numbers/main.go
@@ -1,33 +1,49 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/danstis/lotto-numbers/internal/handlers"
 	"github.com/danstis/lotto-numbers/internal/middleware"
+	"github.com/danstis/lotto-numbers/internal/tracing"
 	"github.com/danstis/lotto-numbers/internal/version"
 	"github.com/gorilla/mux"
+	"go.opentelemetry.io/contrib/instrumentation/github.com/gorilla/mux/otelmux"
+	"go.opentelemetry.io/otel/attribute"
 )
 
-// Main entry point for the app.
+// Main entry point for the app
 func main() {
 	log.Printf("Lotto Numbers started - v%v", version.Version)
+
+	// Start Tracing.
+	ctx := context.Background()
+	tracer, shutdown := tracing.SetupTracing()
+	defer shutdown(ctx)
+
+	ctx, main := tracer.Start(ctx, "startup")
+	defer main.End()
 
 	// Set up the HTTP server using Gorilla Mux
 	r := mux.NewRouter()
 
-	// Wrap the router with the logging middleware
+	// Wrap the router with the logging and tracing middlewares
+	r.Use(otelmux.Middleware("lotto-numbers"))
 	r.Use(middleware.LoggingMiddleware)
 
-	handlers.SetupRoutes(r)
+	handlers.SetupRoutes(ctx, r)
 
 	// Retrieve the port number from an environment variable or use 8080 as default
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8091"
 	}
+	main.SetAttributes(attribute.String("port", port))
+
+	main.End()
 
 	// Start the server
 	log.Printf("Starting server on :%s", port)
---
+++ internal/tracing/tracing.go
@@ -0,0 +1,38 @@
+package tracing
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/danstis/lotto-numbers/internal/version"
+	"github.com/uptrace/uptrace-go/uptrace"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/trace"
+)
+
+func SetupTracing() (trace.Tracer, func(ctx context.Context)) {
+	// Retrieve the uptrace environment variables.
+	dsn := os.Getenv("UPTRACE_DSN")
+	if dsn == "" {
+		log.Fatalf("UPTRACE_DSN environment variable not set")
+	}
+	environment := os.Getenv("ENVIRONMENT")
+	if environment == "" {
+		environment = "development"
+	}
+
+	// Create a new tracer
+	// Note: the service name must match the name of the service in Uptrace
+	// Note: the DSN must be set in the UPTRACE_DSN environment variable
+	uptrace.ConfigureOpentelemetry(
+		uptrace.WithDSN(dsn),
+		uptrace.WithServiceName("lotto-numbers"),
+		uptrace.WithServiceVersion(version.Version),
+		uptrace.WithDeploymentEnvironment(environment),
+	)
+
+	tracer := otel.Tracer("lotto-numbers")
+
+	return tracer, func(ctx context.Context) { uptrace.Shutdown(ctx) }
+}
